7189eaff1c1282f66bdbcf95afd14ba5eb2927c0
RequiemOfSpirit <requiemofspirit2477@gmail.com>
2026-06-15 10:48:59 -0700

    feat(config): Add config for 100% Item Completion board

--- scripts/config.py
+++ scripts/config.py
@@ -41,34 +41,34 @@ MB_BB_ID_TO_COUNT_MAPPING = {
 ALTTP_CE_BOARD_ID = 'm1zjyk06'
 
 # TODO: Fill this:
-# - Create new category and fill id in here
-# - Create BB variable for category if applicable. Find new variable ID from variables.json.
-# - Add values. Filter GetGameData on variableId to get ids for all added values.
+# - Create new category and fill category id in here (id can be found in the URL on the category page)
+# - Create BB variable for category if applicable. Open the new variable and hit save without making any changes.
+#   Read the resulting PutVariableUpdate payload in the network tab to get the variable id and value ids.
 NEW_CE_CATEGORIES = {
     DEFEAT_GANON_RAM_PREP: {
         'id': 'n2ypzpe2',
         'bb_variable_id': None,
     },
     HUNDO_ITEM_COMPLETION: {
-        'id': '',
-        'bb_variable_id': '',
+        'id': 'wk6x59ek',
+        'bb_variable_id': '0nwd90k8',
         'bb_count_to_id_mapping': {
-            '0': '',
-            '1': '',
-            '2': '',
-            '3': '',
-            '4': '',
-            '5': '',
-            '6': '',
-            '7': '',
-            '8': '',
-            '9': '',
-            '10': '',
-            '11': '',
-            '12': '',
-            '13': '',
-            '14': '',
-            '15': '',
+            '0': 'ln896jdl',
+            '1': 'lmopj041',
+            '2': '1w49wx5q',
+            '3': 'qox9w62q',
+            '4': '139pen31',
+            '5': 'qvv9w5wq',
+            '6': 'le25g7zl',
+            '7': 'q5vpejnl',
+            '8': 'lx59w7r1',
+            '9': '14ope0jq',
+            '10': '192kgokq',
+            '11': '12vpeykq',
+            '12': '1py2wmk1',
+            '13': 'qke5kyyq',
+            '14': 'q75pemy1',
+            '15': '1gn5rzml',
         },
     },
     ANY_PERCENT_NO_OOB: {
